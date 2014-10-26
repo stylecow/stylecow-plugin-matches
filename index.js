d07a1b4ebd430b205a15c5bfb56c9746abba8b1f
oscarotero <oom@oscarotero.com>
2014-10-26 12:53:36 +0100

    changes for stylecow@3.0

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ module.exports = function (stylecow) {
 	stylecow.addTask({
 		"Function": {
 			":matches": function (fn) {
-				var selector = fn.ancestor('Selector');
-				var value = selector.name;
+				var selector = fn.ancestor({type: 'Selector'});
+				var value = selector.toString();
 				var search = fn.toString();
 
-				fn.content.forEach(function (replace) {
-					selector.insertBefore(stylecow.Selector.create(value.replace(search, replace)));
+				fn.getValue().forEach(function (replace) {
+					selector.insertBefore(new stylecow.Selector()).setValue(value.replace(search, replace));
 				});
 
 				selector.remove();
