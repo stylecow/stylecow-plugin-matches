9fdd45cfa9de3f15dae9462086f838de118917eb
oscarotero <oom@oscarotero.com>
2014-11-02 21:02:12 +0100

    changes for the new api in stylecow

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ module.exports = function (stylecow) {
 	stylecow.addTask({
 		"Function": {
 			":matches": function (fn) {
-				var selector = fn.ancestor({type: 'Selector'});
+				var selector = fn.parent({type: 'Selector'});
 				var value = selector.toString();
 				var search = fn.toString();
 
-				fn.getValue().forEach(function (replace) {
-					selector.insertBefore(new stylecow.Selector()).setValue(value.replace(search, replace));
+				fn.getContent().forEach(function (replace) {
+					selector.before(new stylecow.Selector()).setContent(value.replace(search, replace));
 				});
 
 				selector.remove();
