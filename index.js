760df35581127037f03d376db249a4153c929917
oscarotero <oom@oscarotero.com>
2014-12-22 00:04:29 +0100

    api changes

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 module.exports = function (stylecow) {
 
 	stylecow.addTask({
-		"Function": {
-			":matches": function (fn) {
-				var selector = fn.parent({type: 'Selector'});
-				var value = selector.toString();
-				var search = fn.toString();
+		filter: {
+			type: 'Function',
+			name: ':matches'
+		},
+		fn: function (fn) {
+			var selector = fn.parent({type: 'Selector'});
+			var value = selector.toString();
+			var search = fn.toString();
 
-				fn.forEach(function (replace) {
-					selector.before(stylecow.Selector.createFromString(value.replace(search, replace.toString())));
-				});
+			fn.forEach(function (replace) {
+				selector.before(stylecow.Selector.createFromString(value.replace(search, replace.toString())));
+			});
 
-				selector.remove();
-			}
+			selector.remove();
 		}
 	});
 };
